2afad40d2387fe171d5e2eb0eb6a7836562f35e6
orimoon <kdm4011@naver.com>
2023-09-13 09:55:36 +0900

    0913

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -147,3 +147,22 @@ $(function () {
     $(this).parent().addClass("on");
   });
 });
+
+//brandstory
+$(document).ready(function () {
+  /* FullPage.JS*/
+  $("#fullpage").fullpage({
+    menu: "#fullpage__menu",
+    anchors: ["a-section-1", "a-section-2", "a-section-3, a-section-4"],
+    loopHorizontal: false,
+    verticalCentered: false,
+    controlArrows: false,
+    slidesNavigation: true,
+  });
+
+  // Menu active toggle
+  $("#fullpage__menu").click(function () {
+    $(this).toggleClass("is-active");
+    $("#fullpage").toggleClass("menu-is-active");
+  });
+});
